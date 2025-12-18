057bfdeb21b41f569e4a1c52f9250bce9eca660c
natnael-mata <nati.sami.mata@gmail.com>
2025-12-18 11:57:08 +0300

    Create relationships, add new attributes, remove unnecessary ones, and add new rules

--- Items/models.py
+++ Items/models.py
@@ -19,54 +19,99 @@ class Category(models.Model):
     parent_id = models.CharField(max_length=5)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
-    last_updated_on = models.DateTimeField(auto_now=True)
-    last_updated_by = models.CharField(max_length=100)
+    last_updated_on = models.DateTimeField(auto_now=True, 
+                                           blank= True, 
+                                           null= True)
+    last_updated_by = models.CharField(max_length=100,
+                                       blank= True, 
+                                       null= True)
+
+    def __str__(self):
+        return self.category_name
 
 class Items(models.Model):
     item_id = models.CharField(max_length=10, primary_key=True)
     item_name = models.CharField(max_length=100)
-    description = models.TextField()
-    #category_id 
+    description = models.TextField(blank = True, null = True)
+    category_id = models.ForeignKey (Category, 
+                                     on_delete = models.CASCADE,
+                                     related_name = 'items',
+                                     db_column = 'category_id') 
     uom = models.CharField(max_length=3)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
-    last_updated_on = models.DateTimeField(auto_now=True)
-    last_updated_by = models.CharField(max_length=100)
+    last_updated_on = models.DateTimeField(auto_now=True, 
+                                           blank= True, 
+                                           null= True)
+    last_updated_by = models.CharField(max_length=100,
+                                       blank= True, 
+                                       null= True)
+
+    def __str__(self):
+        return self.item_name
+    
 
 class Items_Status(models.Model):
     status_id = models.AutoField(primary_key=True)
     effective_date = models.DateField()
-    #item_id (FK)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="A")
-    remark = models.TextField()
+    item_id =  models.ForeignKey(Items, 
+                                 on_delete=models.CASCADE,
+                                 db_column = 'item_id',
+                                 related_name= 'status')
+    status = models.CharField(max_length=1, 
+                              choices=STATUS_CHOICES, 
+                              default="A")
+    remark = models.TextField(blank=True, null= True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
-    last_updated_on = models.DateTimeField(auto_now=True)
-    last_updated_by = models.CharField(max_length=100)
+    last_updated_on = models.DateTimeField(auto_now=True, 
+                                           blank= True, 
+                                           null= True)
+    last_updated_by = models.CharField(max_length=100,
+                                       blank= True, 
+                                       null= True)
 
 
 class Items_Price(models.Model):
     price_id = models.AutoField(primary_key=True)
-    #item_id (FK) = 
-    price = models.DecimalField()
+    item_id =  models.ForeignKey(Items, 
+                                 on_delete=models.CASCADE,
+                                 db_column = 'item_id',
+                                 related_name= 'status')
+    unit_price = models.DecimalField()
     effective_date = models.DateField()
-    remark = models.TextField()
+    remark = models.TextField(blank= True, null= True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
-    last_updated_on = models.DateTimeField(auto_now=True)
-    last_updated_by = models.CharField(max_length=100)
+    last_updated_on = models.DateTimeField(auto_now=True, 
+                                           blank= True, 
+                                           null= True)
+    last_updated_by = models.CharField(max_length=100,
+                                       blank= True, 
+                                       null= True)
 
 
 class Trasactions(models.Model):
     trans_id = models.AutoField(primary_key=True)
     trans_type = models.CharField(max_length=3, choices=TRANS_TYPE_CHOICES)
     trans_date = models.DateTimeField(auto_now=True)
-    #item_id (FK)
+    item_id =  models.ForeignKey(Items, 
+                                 on_delete=models.CASCADE,
+                                 db_column = 'item_id',
+                                 related_name= 'status')
     amount = models.DecimalField()
-    approved_on = models.DateTimeField(auto_now=True)
-    approved_by = models.CharField(max_length=100)
-    remark = models.TextField()
+    approved_on = models.DateTimeField(auto_now=True,
+                                       blank= True, 
+                                       null= True)
+    approved_by = models.CharField(max_length=100,
+                                   blank= True, 
+                                   null= True)
+    remark = models.TextField(blank= True, null= True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
-    last_updated_on = models.DateTimeField(auto_now=True)
-    last_updated_by = models.CharField(max_length=100)
+    last_updated_on = models.DateTimeField(auto_now=True, 
+                                           blank= True, 
+                                           null= True)
+    last_updated_by = models.CharField(max_length=100,
+                                       blank= True, 
+                                       null= True)
