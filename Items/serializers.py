101d328effd5701e9085157422237f62fb4f281b
natnael-mata <nati.sami.mata@gmail.com>
2026-01-03 22:17:44 +0300

    Item Recording issue solved by removing depth

--- Items/serializers.py
+++ Items/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Items, Category, Items_Status, STATUS_CHOICES, TRANS_TYPE_CHOICES, Items_Price, Transactions
 
 #Categry Serializers to handle both read and write operations
-class CategoryCreateSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['category_id', 
@@ -14,20 +14,10 @@ class CategoryCreateSerializer(serializers.ModelSerializer):
                             'created_by',
                             'last_updated_by',
 ]
-
-#Category Serializer for read operations         
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['category_id', 
-                  'category_name',
-                  'parent_id', 
-                  'description', 
-                  'created_on', 
-                  'created_by', 
-                  'last_updated_on', 
-                  'last_updated_by']
-        read_only_fields = fields
+        extra_kwargs = {
+            'category_id': {'required': True},
+            'category_name': {'required': True},
+        }
 
 #Items Serializers for read and write operations        
 class ItemsSerializer(serializers.ModelSerializer):
@@ -45,6 +35,11 @@ class ItemsSerializer(serializers.ModelSerializer):
                             'created_by',
                             'last_updated_by',
                             ]
+        extra_kwargs = {
+                'item_id': {'required': True},
+                'item_name': {'required': True},
+                'category_id': {'required': True},
+        }
 #Items Report Serializer for detailed read operations
 class ItemReportSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='Category.category_name', read_only=True)
@@ -70,11 +65,7 @@ class ItemsStatusSerializer(serializers.ModelSerializer):
                   'status', 
                   'remark', 
                   'created_on', 
-                  'created_by', 
-                  'last_updated_on', 
-                  'last_updated_by']
-        depth = 1  # To include related Items details       
-
+        ]
         read_only_fields = ['created_on',
                             'last_updated_on',
                             'created_by',
