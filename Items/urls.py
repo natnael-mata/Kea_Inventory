ee175c9c801eedb7e96735aa52e9dc84c0b14247
natnael-mata <nati.sami.mata@gmail.com>
2026-01-03 23:49:10 +0300

    Serilizer rule defined for tansaction and new data model added initial balance for flexablity

--- Items/urls.py
+++ Items/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ItemsViewSet, ItemsStatusViewSet, ItemsListViewSet
+from .views import CategoryViewSet, ItemsViewSet, ItemsStatusViewSet, ItemsListViewSet, ItemsPriceViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'items', ItemsViewSet)
-router.register(r'items-list', ItemsListViewSet, basename='items-list')
 router.register(r'items-status', ItemsStatusViewSet)
+router.register(r'items-price', ItemsPriceViewSet)
+router.register(r'items-list', ItemsListViewSet, basename='items-list')
 
 urlpatterns = [
     path('', include(router.urls)),
